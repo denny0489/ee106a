364d8025eaf23deb5a3d68ba7c7c83b3bfbb885c
Amanda Boyle <amandalynn@berkeley.edu>
2018-12-02 14:07:27 -0800

    update ik.py
    trying to get it communicating with arduino

--- Code/src/mv/src/ik.py
+++ Code/src/mv/src/ik.py
@@ -8,7 +8,8 @@ from numpy import linalg
 from baxter_interface import gripper as robot_gripper
 from tf.msg import tfMessage
 from tf.transformations import quaternion_from_euler
-import serial #To install
+import math
+from serial import * #To install
 
 # https://arduino.stackexchange.com/questions/45874/arduino-serial-communication-with-python-sending-an-array
 
@@ -24,28 +25,50 @@ def main():
         raw_input('Press [ Enter ]: ')
         
         try: 
-            arduino = serial.Serial("/dev/ttyACMO", timeout = 1)
+            arduino = serial.Serial("/dev/ttyACM0", 115200)
         except:
             print("Error when getting data from arduino")
             continue;
 
-        if (arduino.readLine().length() == 0):
+	while True:
+            print("Data from arduino = " + arduino.readline().decode("utf-8"))
+
+        if (len(arduino.readline()) == 0):
             print ("No data from arduino")
             continue
 
         #If decoding is needed
-        # decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+        #decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
         # print(decoded_bytes)
-        data = []
-        data = str(arduino.readLine()).split(" ")
+        data1 = arduino.readline().decode("utf-8")
+	print(i for i in data1)
+	data = [float(i) for i in data1]
+	#print("x1, y1, x2, y2", data[0],data[1],data[2],data[3],data[4],data[5])
+	# data = [x1, y1, x2, y2, imu_yaw(z), imu_pitch(y), imu_roll(x)]
+	# x_meters = x_pixels *0.609 / 1000
+	# y_meters = y_pixels *0.609 / 750
 
-        # data format: 
-        # space separated data 
-        # length = 6 
-        # [position.x position.y position.z orientation.x orientation.y orientation.z]
+	# -x1_world 		= y_baxter		in pixels
+	pos_y = data[0] / 1000
+	
+	# -(1/2)(y1 + y2)_world = z_baxter		in pixels
+	pos_z = (1/2)*(data[1] + data[3]) / 750
 
-        # Get input from Arduino
-        # final position x, y, z, orientation x, y, z
+	# x2_world 		= x_baxter		in pixels
+	pos_x = data[2] / 1000
+
+	# roll, pitch in degrees [-180, 180]
+	# roll sign is inverted and needs to be flipped
+	# yaw in degrees [0, 360]
+	roll = math.radians(data[6])
+	pitch = math.radians(data[5])
+	yaw = data[4]
+	yaw -= 140
+	if yaw > 180:
+	    yaw -= 360
+	yaw = math.radians(yaw)
+	
+	
 
         #Construct the request
         request = GetPositionIKRequest()
@@ -55,25 +78,27 @@ def main():
         request.ik_request.pose_stamped.header.frame_id = "base"
         
         #Set the desired orientation for the end effector HERE
-	    # x is forward/backward
-        request.ik_request.pose_stamped.pose.position.x = float(data[0])
-	    # y is left/right
-        request.ik_request.pose_stamped.pose.position.y = float(data[1])
-	    # z is up/down
-        request.ik_request.pose_stamped.pose.position.z = float(data[2])
+	# x is forward/backward
+        request.ik_request.pose_stamped.pose.position.x = pos_x
+	# y is left/right
+        request.ik_request.pose_stamped.pose.position.y = pos_y
+	# z is up/down
+        request.ik_request.pose_stamped.pose.position.z = pos_z
 
-	    # orientation given from IMU in radians (roll, pitch, yaw)
+	# orientation given from IMU in radians (roll, pitch, yaw)
         # convert from (roll, pitch, yaw) into quaternion
-	    orient_quat = quaternion_from_euler(float(data[3]), float(data[4]), float(data[5]))
-	    # x is roll
+	orient_quat = quaternion_from_euler(roll, pitch, yaw)
+	# x is roll
         request.ik_request.pose_stamped.pose.orientation.x = orient_quat[0]
-	    # y is pitch
+	# y is pitch
         request.ik_request.pose_stamped.pose.orientation.y = orient_quat[1]
-	    # z is yaw
+	# z is yaw
         request.ik_request.pose_stamped.pose.orientation.z = orient_quat[2]
-	    # negate w for inverse
+	# negate w for inverse
         request.ik_request.pose_stamped.pose.orientation.w = orient_quat[3]
-        
+
+	print("x:, y:, z:", pos_x, pos_y, pos_z, "orientation", orient_quat[0], orient_quat[1], orient_quat[2], orient_quat[3])        
+
         try:
             #Send the request to the service
             response = compute_ik(request)
@@ -112,4 +137,3 @@ def main():
 #Python's syntax for a main() method
 if __name__ == '__main__':
     main()
-
