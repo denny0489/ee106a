87a2aa02e21cd3bcee7935068df7d0ad848ad50f
AmandaLynn79 <amandalynn@berkeley.edu>
2018-12-06 15:26:53 -0800

    cleaned up

--- Code/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/cc/ee106a/fa18/class/ee106a-aaj/ros_workspaces/lab5/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/cc/ee106a/fa18/class/ee106a-aaj/ros_workspaces/lab5/devel;/scratch/shared/baxter_ws/devel;/opt/ros/indigo".split(';') if "/home/cc/ee106a/fa18/class/ee106a-aaj/ros_workspaces/lab5/devel;/scratch/shared/baxter_ws/devel;/opt/ros/indigo" != "" else []
--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# ee106a
+# UC Berkeley, EE106A, Fall 2018
 Final Project for EE106A
+
 Team Members: 
 Amanda Boyle
-Denny Min
+Syung (Denny) Min
 Daniel Benniah John
-Haopeng Liang
+Haopeng (Jason) Liang
 Junlin Liu
+
+Abstract:
+The UltraBaxter's main goal is to be able to directly manipulate the Baxter's joints 
+to mimic the user's arm motion by using various sensors to track the user's arm's 
+joint positions and translating the sensor measurements to Baxter's joints.
